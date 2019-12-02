c1c0e073b293dab90379e343743d641c814f2a6d
Zompaktu <1vs1@mail.ru>
2019-12-02 16:35:39 +0500

    Correct TXT record deletion

--- yandex-cleanup-hook.sh
+++ yandex-cleanup-hook.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
-
 _dir="$(dirname "$0")"
 source "$_dir/config.sh"
 
-if [ -f /tmp/CERTBOT_${CERTBOT_DOMAIN}/${CERTBOT_VALIDATION} ]; then
-        RECORD_ID=$(cat /tmp/CERTBOT_${CERTBOT_DOMAIN}/${CERTBOT_VALIDATION})
-        rm -f /tmp/CERTBOT_${CERTBOT_DOMAIN}/${CERTBOT_VALIDATION}
-fi
-
-#DOMAIN=$(expr match "$CERTBOT_DOMAIN" '.*\.\(.*\..*\)')
-DOMAIN=$(echo "$CERTBOT_DOMAIN" | awk -F'.' '{print $(NF-1)"."$NF}')
-if [ -z "${DOMAIN}" ]; then
-        echo "CERTBOT_DOMAIN=${CERTBOT_DOMAIN}, DOMAIN=${DOMAIN}, BAD DOMAIN, exit"
-        exit
-fi
-
-API_KEY=${PDDTOKENMAP["$DOMAIN"]}
-if [ -z "${API_KEY}" ]; then
-        echo "No API_KEY for DOMAIN=${DOMAIN}, exit"
-        exit
+# Get API key for current domain
+API_KEY=${API_KEYMAP["$CERTBOT_DOMAIN"]}
+if [ -z "$API_KEY" ]; then
+       echo "No API key found for domain $CERTBOT_DOMAIN, exit"
+       exit
 fi
 
 # Remove the challenge TXT record from the zone
+remove_record() {
+        RECORD_ID="$1"
 if [ -n "${RECORD_ID}" ]; then
         RESULT=$(curl -s -X POST "https://pddimp.yandex.ru/api2/admin/dns/del" \
-     -H "PddToken: $API_KEY" \
-     -d "domain=$DOMAIN&record_id=$RECORD_ID" \
+        -H "PddToken: $API_KEY" \
+        -d "domain=$CERTBOT_DOMAIN&record_id=$RECORD_ID" \
          | python -c "import sys,json;print(json.load(sys.stdin)['success'])")
+        echo $RESULT
+fi
+}
 
-        echo $RESULT 
+if [ -f /tmp/CERTBOT_$CERTBOT_DOMAIN/RECORD_ID ]; then
+        while read RECORD; do
+                remove_record $RECORD
+        done < /tmp/CERTBOT_$CERTBOT_DOMAIN/RECORD_ID
+        rm -f /tmp/CERTBOT_$CERTBOT_DOMAIN/RECORD_ID
 fi
