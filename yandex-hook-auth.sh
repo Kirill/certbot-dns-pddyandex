3d13829eebd7eb2939ea6572b59d69646b641cf9
Dmitriy Safronov <zimniy@cyberbrain.pw>
2022-04-11 17:22:08 +0300

    fix logger

--- yandex-hook-auth.sh
+++ yandex-hook-auth.sh
@@ -3,6 +3,10 @@
 _dir="$(dirname "$0")"
 source "${HOME}/.certbot-dns-pddyandex.rc" || exit 1
 
+my_logger() {
+        echo "$1" > /dev/stderr
+}
+
 # Get API key for current domain
 API_KEY=${API_KEYMAP["$CERTBOT_DOMAIN"]}
 if [ -z "$API_KEY" ]; then
@@ -13,9 +17,9 @@ fi
 # Create TXT record
 CREATE_DOMAIN="_acme-challenge"
 RECORD_ID=$(curl -s -X POST "https://pddimp.yandex.ru/api2/admin/dns/add" \
-     -H "PddToken: $API_KEY" \
-     -d "domain=$CERTBOT_DOMAIN&type=TXT&content=$CERTBOT_VALIDATION&ttl=300&subdomain=$CREATE_DOMAIN" \
-      | python3 -c "import sys,json;print(json.load(sys.stdin)['record']['record_id'])")
+        -H "PddToken: $API_KEY" \
+        -d "domain=$CERTBOT_DOMAIN&type=TXT&content=$CERTBOT_VALIDATION&ttl=300&subdomain=$CREATE_DOMAIN" \
+        | python3 -c "import sys,json;print(json.load(sys.stdin)['record']['record_id'])")
 
 # Save info for cleanup
 if [ ! -d /tmp/CERTBOT_$CERTBOT_DOMAIN ];then
@@ -26,51 +30,51 @@ else
 fi
 
 unset DNS_GOOGLE DNS_CLOUDFLARE DNS_OPENDNS
-logger -t certbot-dns-pddyandex -s "Starting"
+my_logger "Starting"
 # Sleep to make sure the change has time to propagate over to DNS (max: 20 min)
 c_time=0
 end_time=86400
 while [ "$c_time" -le "$end_time" ]; do
         if [ `dig $CREATE_DOMAIN.$CERTBOT_DOMAIN TXT +short @dns1.yandex.net | grep $CERTBOT_VALIDATION` ]; then
-                logger -t certbot-dns-pddyandex -s "Yandex: Primary - yes"
+                my_logger "Yandex: Primary - yes"
                 sleep 5
                 if [ `dig $CREATE_DOMAIN.$CERTBOT_DOMAIN TXT +short @dns2.yandex.net | grep $CERTBOT_VALIDATION` ]; then
-                        logger -t certbot-dns-pddyandex -s "Yandex: Secondary - yes"
+                        my_logger "Yandex: Secondary - yes"
                         sleep 5
                         if [ `dig $CREATE_DOMAIN.$CERTBOT_DOMAIN TXT +short @8.8.8.8 | grep $CERTBOT_VALIDATION` ]; then
-                                logger -t certbot-dns-pddyandex -s "Propagation: Google - yes"
+                                my_logger "Propagation: Google - yes"
                                 DNS_GOOGLE=1
                         else
-                                logger -t certbot-dns-pddyandex -s "Propagation: Google - no"
+                                my_logger "Propagation: Google - no"
                         fi
                         if [ `dig $CREATE_DOMAIN.$CERTBOT_DOMAIN TXT +short @1.1.1.1 | grep $CERTBOT_VALIDATION` ]; then
-                                logger -t certbot-dns-pddyandex -s "Propagation: Cloudflare - yes"
+                                my_logger "Propagation: Cloudflare - yes"
                                 DNS_CLOUDFLARE=1
                         else
-                                logger -t certbot-dns-pddyandex -s "Propagation: Cloudflare - no"
+                                my_logger "Propagation: Cloudflare - no"
                         fi
                         if [ `dig $CREATE_DOMAIN.$CERTBOT_DOMAIN TXT +short @208.67.222.222 | grep $CERTBOT_VALIDATION` ]; then
-                                logger -t certbot-dns-pddyandex -s "Propagation: OpenDNS - yes"
+                                my_logger "Propagation: OpenDNS - yes"
                                 DNS_OPENDNS=1
                         else
-                                logger -t certbot-dns-pddyandex -s "Propagation: OpenDNS - no"
+                                my_logger "Propagation: OpenDNS - no"
                         fi
                         if [ "${DNS_GOOGLE}" = '1' ] && [ "${DNS_CLOUDFLARE}" = '1' ] && [ "${DNS_OPENDNS}" = '1' ]; then
-                                logger -t certbot-dns-pddyandex -s "Completed"
+                                my_logger "Completed"
                                 break
                         else
-                                logger -t certbot-dns-pddyandex -s "In progress"
+                                my_logger "In progress"
                                 sleep 55
                                 c_time=$[c_time+55]
                         fi
                         unset DNS_GOOGLE DNS_CLOUDFLARE DNS_OPENDNS
                 else
-                        logger -t certbot-dns-pddyandex -s "Yandex: Secondary - no"
+                        my_logger "Yandex: Secondary - no"
                         sleep 55
                         c_time=$[c_time+55]
                 fi
         else
-                logger -t certbot-dns-pddyandex -s "Yandex: Primary - no"
+                my_logger "Yandex: Primary - no"
                 sleep 60
                 c_time=$[c_time+60]
         fi
